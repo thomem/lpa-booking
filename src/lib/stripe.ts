41140999cdf6caebb60a505e5944f9d11c73585c
parlementjpv <parlementjpv@gmail.com>
2026-06-29 19:19:56 +0200

    Fix Stripe API version to 2025-02-24

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,5 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-10-28.acacia",
-  typescript: true,
+  apiVersion: "2025-02-24.acacia",
 });
